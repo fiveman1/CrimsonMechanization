01efba62689ba86511a77d4bc408fe542124c8cd
Matthew Dixey <dixey001@umn.edu>
2021-01-07 21:20:36 -0600

    Added Block of Crimson

---
+++ src/main/java/fiveman1/crimsonmechanization/blocks/BlockCrimson.java
@@ -0,0 +1,9 @@
+package fiveman1.crimsonmechanization.blocks;
+
+import net.minecraft.block.material.Material;
+
+public class BlockCrimson extends BlockBase {
+    public BlockCrimson(String name){
+        super(name, Material.IRON);
+    }
+}
--- src/main/java/fiveman1/crimsonmechanization/blocks/ModBlocks.java
+++ src/main/java/fiveman1/crimsonmechanization/blocks/ModBlocks.java
@@ -4,9 +4,11 @@ public class ModBlocks {
 
     public static BlockBart blockBart;
     public static BlockCrimsonFurnace blockCrimsonFurnace;
+    public static BlockCrimson blockCrimson;
 
     public static void init() {
         blockBart = new BlockBart("bart_block");
         blockCrimsonFurnace = new BlockCrimsonFurnace("crimson_furnace");
+        blockCrimson = new BlockCrimson("crimson_block");
     }
 }
