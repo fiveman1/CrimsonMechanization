245eea688113130af3b636cd067380197113f8b8
Matthew Dixey <dixey001@umn.edu>
2021-01-15 13:47:41 -0600

    Added the Crimson Iron and Crimson Steel armor sets. Also added some abstraction to the ItemArmorBase class.

--- src/main/java/fiveman1/crimsonmechanization/items/armor/ItemArmorBase.java
+++ src/main/java/fiveman1/crimsonmechanization/items/armor/ItemArmorBase.java
@@ -11,19 +11,25 @@ import javax.annotation.Nullable;
 
 public class ItemArmorBase extends ItemArmor {
 
+    protected String armor_material;
+
     public ItemArmorBase(String name, EntityEquipmentSlot equipmentSlotIn) {
         super(ArmorMaterial.DIAMOND, 1, equipmentSlotIn);
         RegistryHandler.initItem(this, name);
         setMaxStackSize(1);
+
+        int index = name.indexOf("_");
+        armor_material = name.substring(0, index);
+
     }
 
     @Nullable
     @Override
     public String getArmorTexture(ItemStack stack, Entity entity, EntityEquipmentSlot slot, String type) {
         if (slot == EntityEquipmentSlot.LEGS){
-            return CrimsonMechanization.MODID + ":textures/models/armor/crimson_model_layer_2.png";
+            return CrimsonMechanization.MODID + ":textures/models/armor/" + armor_material + "_model_layer_2.png";
         }
-        return CrimsonMechanization.MODID + ":textures/models/armor/crimson_model_layer_1.png";
+        return CrimsonMechanization.MODID + ":textures/models/armor/" + armor_material + "_model_layer_1.png";
     }
 
 }
