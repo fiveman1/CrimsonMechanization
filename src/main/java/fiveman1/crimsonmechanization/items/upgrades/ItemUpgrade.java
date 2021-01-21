9ce54bf63b49e609a7cc7ff9fa04e89b70bbb253
Carter Penterman <carterpenterman@gmail.com>
2021-01-20 20:59:08 -0600

    added localization and tooltips for upgrades

--- src/main/java/fiveman1/crimsonmechanization/items/upgrades/ItemUpgrade.java
+++ src/main/java/fiveman1/crimsonmechanization/items/upgrades/ItemUpgrade.java
@@ -4,13 +4,21 @@ import fiveman1.crimsonmechanization.CrimsonMechanization;
 import fiveman1.crimsonmechanization.util.IInitializeable;
 import fiveman1.crimsonmechanization.util.RegistryHandler;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
 
 public class ItemUpgrade extends Item implements IInitializeable {
 
@@ -19,8 +27,24 @@ public class ItemUpgrade extends Item implements IInitializeable {
     public ItemUpgrade(EnumUpgrade enumUpgrade) {
         upgradeType = enumUpgrade;
         setRegistryName(CrimsonMechanization.MODID, "upgrade_" +  upgradeType.resourceName);
-        setUnlocalizedName("upgrade" + upgradeType.unlocalizedName);
+        setUnlocalizedName(CrimsonMechanization.MODID + ".upgrade" + upgradeType.unlocalizedName);
         RegistryHandler.INITIALIZEABLES.add(this);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        Item item = stack.getItem();
+        if (item instanceof ItemUpgrade) {
+            EnumUpgrade upgradeType = ((ItemUpgrade) item).upgradeType;
+            if (upgradeType == EnumUpgrade.EFFICIENCY) {
+                tooltip.add(I18n.format("messages.crimsonmechanization.upgrade_efficiency"));
+            } else if (upgradeType == EnumUpgrade.SPEED) {
+                Collections.addAll(tooltip, I18n.format("messages.crimsonmechanization.upgrade_speed1"),
+                        I18n.format("messages.crimsonmechanization.upgrade_speed2"));
+            } else if (upgradeType == EnumUpgrade.LUCK) {
+                tooltip.add(I18n.format("messages.crimsonmechanization.upgrade_luck"));
+            }
+        }
     }
 
     @Override
